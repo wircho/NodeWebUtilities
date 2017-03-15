3ebb662f69aa28c3847553ec719c3eb3bf7a5f80
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2017-03-15 17:18:42 -0400

    logging response body on end

--- index.js
+++ index.js
@@ -201,8 +201,8 @@ var RequestBackEndHelpers = {
         body += d;
       }.bind(this));
       response.on("end", function(d) {
+        console.log("From request got response body: " + body);
         if (this.responseType === "json") {
-          console.log("From request got response body: " + body);
           var json = undefined;
           try {
             json = JSON.parse(body);
